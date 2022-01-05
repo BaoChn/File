ae0120c0d92746965b62ad1fef9d9dbfbac527aa
宝昌 <32792217+BaoChn@users.noreply.github.com>
2022-01-05 20:30:57 +0800

    Add files via upload

--- Script/Nav_Bar.js
+++ Script/Nav_Bar.js
@@ -4,8 +4,8 @@ var x = true;
 $("html").click(function(){
     if(x){
         // 头部添加
-        var title = document.title;
-        var headerh = '<h3>'+title+'</h3><div><img src="https://a-b.cc/wp-content/uploads/iconder.webp" alt="[ '+title+' ]"/></div><p>一个汇集艺术创作素材的元素小宇宙</p><ul><li><a href="/">站点主页</a></li><li><a href="www.a-b.cc">网站导航</a></li><li><a href="https://pan.baidu.com/s/4mqNq3n6">参与分享</a></li><li><a href="https://a-b.cc">个人博客</a></li></ul>';
+        var title = "素材宝库";
+        var headerh = '<h3>'+title+'</h3><div class="avatar"><a target="_blank" href="https://a-b.cc"><img src="https://A-B.CC/wp-content/uploads/iCool.svg" title="前往宝秘基地🛸"></a></div><p>一个汇集艺术创作素材的元素小宇宙</p><ul><li><a href="/" title="返回本站首页🔙">🏠主页面</a></li><li><a href="https://www.a-b.cc" title="一个网址导航🧭">🚀导航站</a></li><li><a href="https://pan.baidu.com/s/4nyfYvsH" title="共筑美好未来💫">💎同参与</a></li><li><a href="https://i.a-b.cc/comment.html" title="分享奇思妙想🤯">📝留言栏</a></li></ul>';
         $(".header").append(headerh);
     }
 })
